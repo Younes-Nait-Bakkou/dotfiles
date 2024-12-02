adaf9f992ecdf2adc525170571cf4c006160e5a1
YounesNaitBakkou <younes.naitbakkou@automize.ma>
2024-12-02 23:15:12 +0100

    Add better-escape, mini-animate and trouble.nvim

--- private_dot_config/nvim/lua/chadrc.lua
+++ private_dot_config/nvim/lua/chadrc.lua
@@ -18,11 +18,31 @@ M.ui = {
     cmp = {
         icons_left = true, -- only for non-atom styles!
         lspkind_text = true,
-        style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+        style = "default", -- default/flat_light/flat_dark/atom/atom_colored
         format_colors = {
             tailwind = true, -- will work for css lsp too
             icon = "󱓻",
         },
+    },
+
+    telescope = { style = "bordered" }, -- borderless / bordered
+
+    statusline = {
+        enabled = true,
+        theme = "default", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow separators work only for default statusline theme
+        -- round and block will work for minimal theme only
+        separator_style = "default",
+        order = nil,
+        modules = nil,
+    },
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+        enabled = true,
+        lazyload = true,
+        order = { "treeOffset", "buffers", "tabs", "btns" },
+        modules = nil,
     },
 }
 
