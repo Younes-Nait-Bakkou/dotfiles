15cde8b167f97df7c34bb524d97fa7734923f82d
Younes Nait Bakkou <naitbakkou.younes@gmail.com>
2024-12-02 23:15:12 +0100

    Add better-escape, mini-animate and trouble.nvim

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -317,4 +317,58 @@ return {
         "tpope/vim-fugitive",
         event = { "BufEnter" },
     },
+
+    -- Subtle, yet eye-easing animations
+    {
+        "echasnovski/mini.animate",
+        version = false,
+        event = { "BufEnter" },
+    },
+
+    -- Ways to display and navigate diagnostics
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+
+    -- lua with lazy.nvim
+    {
+        "max397574/better-escape.nvim",
+        config = function()
+            require("configs.better-escape")
+        end,
+    },
 }
