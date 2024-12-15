4f62277feea66cebae7bc9a94bf0d2d4d1b3c9ba
YounesNaitBakkou <younes.naitbakkou@automize.ma>
2024-12-15 15:34:44 +0100

    Configured mini-ai tested on lua and workin

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -394,8 +394,22 @@ return {
             require("configs.harpoon")
         end,
     },
+
     {
         "echasnovski/mini.ai",
-        event = { "BufEnter" },
+        event = "VeryLazy",
+        keys = {
+            { "a", mode = { "x", "o" } },
+            { "i", mode = { "x", "o" } },
+            { "g", mode = { "x", "o" } },
+        },
+        dependencies = {
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+            },
+        },
+        config = function()
+            require("configs.mini-ai")
+        end,
     },
 }
