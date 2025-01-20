0c9e1518551d372936c74179531b0b9d08795a23
Younes Nait Bakkou <naitbakkou.younes@gmail.com>
2025-01-20 10:45:08 +0100

    Add 256 color support to tmux, neovim and kitty and add harper_ls to neovim

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -11,3 +11,12 @@ o.tabstop = 4
 o.softtabstop = 4
 o.scrolloff = 10
 vim.g.material_style = "darker"
+-- o.termguicolors = true
+
+-- Enable Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Enable neovim built-in spell check
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
