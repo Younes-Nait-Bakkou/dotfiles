393740fa008944108262f79d8b3957833cc2eb4f
YounesNaitBakkou <younes.naitbakkou@automize.ma>
2025-11-16 15:52:27 +0100

    feat(nvim): add markdown preview

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -592,4 +592,13 @@ return {
             { "<leader>ac", mode = "n", "<cmd>LLMSessionToggle<cr>" },
         },
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 }
