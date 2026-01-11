9ddcfccb06480310f5460f42a8b729be7c30f2b1
Younes Nait Bakkou <naitbakkou.younes@gmail.com>
2026-01-11 22:50:56 +0100

    feat(neovim): style lsp diagnostics

--- roles/neovim/files/nvim/lua/configs/lspconfig.lua
+++ roles/neovim/files/nvim/lua/configs/lspconfig.lua
@@ -357,3 +357,27 @@ end
 for _, lsp in ipairs(enabled_servers) do
     vim.lsp.enable(lsp)
 end
+
+vim.diagnostic.config({
+    virtual_lines = true,
+    -- virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
