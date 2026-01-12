15f9febcabd5e3ad4d8d8b35f10dba94d0df7ceb
Younes Nait Bakkou <naitbakkou.younes@gmail.com>
2026-01-12 19:48:41 +0100

    Feat(neovim): Configure nvim to auto-convert line endings to LF

--- roles/neovim/files/nvim/lua/options.lua
+++ roles/neovim/files/nvim/lua/options.lua
@@ -14,6 +14,16 @@ o.scrolloff = 10
 vim.g.material_style = "darker"
 -- o.termguicolors = true
 
+-- File format settings
+o.fileformats = "unix,dos,mac" -- Order of detection
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*",
+    callback = function()
+        vim.opt.fileformat = "unix"
+    end,
+})
+
 -- Enable Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
