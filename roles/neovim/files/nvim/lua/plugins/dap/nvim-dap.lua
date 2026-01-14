ecd4bb76dfcc9f497aa78c6ccea690ac8bb9a413
Younes Nait Bakkou <naitbakkou.younes@gmail.com>
2026-01-14 14:50:09 +0100

    refactor(neovim): move up config function in dap nvim

--- roles/neovim/files/nvim/lua/plugins/dap/nvim-dap.lua
+++ roles/neovim/files/nvim/lua/plugins/dap/nvim-dap.lua
@@ -1,12 +1,13 @@
 return {
     "mfussenegger/nvim-dap",
+    config = function()
+        require("configs.dap")
+    end,
     dependencies = {
         "rcarriga/nvim-dap-ui",
         "theHamsta/nvim-dap-virtual-text",
         "nvim-neotest/nvim-nio",
         "williamboman/mason.nvim",
     },
-    config = function()
-        require("configs.dap")
-    end,
 }
+
